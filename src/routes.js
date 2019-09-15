2585c2248e7cb864dd885e824dd7ac7f912bc5b1
Lucas Ferreira <lucasfsouza182@gmail.com>
2019-09-15 12:00:08 -0300

    Add column avatar_id on user and File model

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middleware/auth';
 import multerConfig from './config/multer';
@@ -17,8 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
