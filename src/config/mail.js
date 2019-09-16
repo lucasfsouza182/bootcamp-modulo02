771d8005bdd1e2f8662c687f5c08bdb47de6ed53
Lucas Ferreira <lucasfsouza182@gmail.com>
2019-09-15 21:24:48 -0300

    Included mail when the appointment was canceled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7c727c9adf1845',
+    pass: '24f8d2e350afdc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
