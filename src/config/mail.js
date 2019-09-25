3fcac8fd66e2e90f03272edab58c9ba87e3d3f65
Lucas Ferreira <lucasfsouza182@gmail.com>
2019-09-24 22:25:04 -0300

    Add dotenv on project

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '7c727c9adf1845',
-    pass: '24f8d2e350afdc',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
